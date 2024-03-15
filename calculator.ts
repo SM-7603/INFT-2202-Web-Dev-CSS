a2183d3918ab92cdc3ac21be04d0704a1b09c548
Shubham Mohanty <mohanty.shubham007@gmail.com>
2024-03-14 21:51:47 -0400

    feat: :sparkles: add calculator.ts
    all we've really done here is change let to const, add the data type for the parameters, just state the module exports directly.

---
+++ calculator.ts
@@ -0,0 +1,22 @@
+const add = (a: number, b: number): number => {
+    return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+    return a - b;
+};
+
+const multiply = (a: number, b: number): number => {
+    return a * b;
+};
+
+const divide = (a: number, b: number): number => {
+    return a / b;
+};
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+};
